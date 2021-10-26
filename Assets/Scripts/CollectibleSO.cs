de62b84cefc53426d145cea10671ed4b35b908c0
气泡缓释胶囊 <japanztp@gmail.com>
2021-10-26 14:23:52 +0800

    Function enhancement
    Collectible is now available.

---
+++ Assets/Scripts/CollectibleSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleSO : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemSprite;
+    public string itemDescription;
+}
