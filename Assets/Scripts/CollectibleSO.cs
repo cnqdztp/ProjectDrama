ffc94482b1befa08e268038b31660ae8783702c1
Sessyoinpornhob <71918373+Sessyoinpornhob@users.noreply.github.com>
2021-10-26 18:04:19 +0800

    18:04

--- Assets/Scripts/CollectibleSO.cs
+++ Assets/Scripts/CollectibleSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu (menuName ="SO/Collectible")]
 public class CollectibleSO : ScriptableObject
 {
     public string itemName;
